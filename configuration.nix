538fe3a8cf996f8cafd7694c8c50e589a4b434df
kheldae <taho.gougeon@gmail.com>
2025-11-03 11:01:29 +0100

    extra opts

--- configuration.nix
+++ configuration.nix
@@ -7,9 +7,26 @@
 
 let
   system = "x86_64";
+
+  # build quartus ourselves
+
 in
 {
+  # basic environment info
+  system.stateVersion = "25.11";
+
+  # packages to run
   environment.systemPackages = with pkgs; [ cowsay ];
+
+  # custom user
+  users.users."quartus" = {
+    isNormalUser = true;
+    createHome = true;
+    home = "/home";
+    description = "Quartus base user";
+    extraGroups = [ "wheel" ];
+    password = "quartus";
+  };
 
   # no-password sudo on this one
   security.sudo = {
